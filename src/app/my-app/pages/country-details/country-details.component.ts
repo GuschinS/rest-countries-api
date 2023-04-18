5bb226df89343c26bbd2cb25c3d27e8fe5c984f9
guschins <guschins@gmail.com>
2023-04-18 12:01:51 +0200

    feat: add code to country-details component

--- src/app/my-app/pages/country-details/country-details.component.ts
+++ src/app/my-app/pages/country-details/country-details.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { RestCountries } from '../../interfaces/rest-countries';
+import { RestCountriesService } from '../../services/rest-countries.service';
 
 @Component({
   selector: 'app-country-details',
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.scss']
+  styleUrls: ['./country-details.component.scss'],
 })
-export class CountryDetailsComponent {
+export class CountryDetailsComponent implements OnInit {
+  country!: RestCountries;
+  currenciesKey!: string;
+  currencies!: string;
+  flag?: string;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private restCountriesService: RestCountriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap((params) =>
+          this.restCountriesService.getCountryByCode(params['id'])
+        )
+      )
+      .subscribe((country) => {
+        this.country = country[0];
+        this.flag = country[0].flags.png;
+        this.currenciesKey = Object.keys(this.country.currencies)[0];
+        this.currencies = country[0].currencies[this.currenciesKey].symbol;
+      });
+  }
 }
