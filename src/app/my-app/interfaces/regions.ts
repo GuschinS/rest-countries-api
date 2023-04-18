5f9f6659e8698e29a5061c5978c5666cbb92d264
guschins <guschins@gmail.com>
2023-04-18 09:50:32 +0200

    feat: add interfaces

---
+++ src/app/my-app/interfaces/regions.ts
@@ -0,0 +1,27 @@
+export interface Regions {
+  id: number;
+  name: string;
+}
+
+export const regions = [
+  {
+    id: 1,
+    name: 'Afryka',
+  },
+  {
+    id: 2,
+    name: 'Ameryka',
+  },
+  {
+    id: 3,
+    name: 'Azja',
+  },
+  {
+    id: 4,
+    name: 'Europa',
+  },
+  {
+    id: 5,
+    name: 'Oceania',
+  },
+];
