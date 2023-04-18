c38cde72e826ad399fb63d36adfa18c484c53b6f
guschins <guschins@gmail.com>
2023-04-18 11:31:01 +0200

    feat: add code to country component

--- src/app/my-app/pages/country/country.component.ts
+++ src/app/my-app/pages/country/country.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestCountries } from '../../interfaces/rest-countries';
+import { ActivatedRoute } from '@angular/router';
+import { Regions, regions } from '../../interfaces/regions';
+import { RestCountriesService } from '../../services/rest-countries.service';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
-  styleUrls: ['./country.component.scss']
+  styleUrls: ['./country.component.scss'],
 })
-export class CountryComponent {
+export class CountryComponent implements OnInit {
+  region: Regions | undefined;
 
+  selectedRegion: string = '';
+
+  countries: RestCountries[] = [];
+
+  constructor(
+    private restCountriesService: RestCountriesService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const regionIdFromRoute = Number(routeParams.get('regionId'));
+    this.region = regions.find((region) => region.id === regionIdFromRoute);
+
+    let reg: string;
+
+    switch (regionIdFromRoute) {
+      case 1:
+        reg = 'africa';
+        break;
+      case 2:
+        reg = 'america';
+        break;
+      case 3:
+        reg = 'asia';
+        break;
+      case 4:
+        reg = 'europe';
+        break;
+      default:
+        reg = 'oceania';
+        break;
+    }
+
+    this.selectedRegion = reg;
+
+    this.restCountriesService
+      .getCountriesByRegion(this.selectedRegion)
+      .subscribe({
+        next: (reg) => {
+          this.countries = reg;
+        },
+      });
+  }
 }
