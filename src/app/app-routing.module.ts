89e3f94c0cc6f0b89e1b918eb59b9a1c01c9cceb
guschins <guschins@gmail.com>
2023-04-18 09:51:11 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegionComponent } from './my-app/pages/region/region.component';
+import { CountryComponent } from './my-app/pages/country/country.component';
+import { CountryDetailsComponent } from './my-app/pages/country-details/country-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'regions/:regionId', component: CountryComponent },
+  { path: 'country/:id', component: CountryDetailsComponent },
+  { path: '', component: RegionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
