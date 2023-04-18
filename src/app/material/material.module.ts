8fd9ee71e4329e20e3ccd9c8ed09d7a89699be08
guschins <guschins@gmail.com>
2023-04-18 10:44:22 +0200

    feat: mat-dialog-module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatCommonModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
-  imports: [MatCommonModule, MatButtonModule],
-  exports: [MatCommonModule, MatButtonModule],
+  imports: [MatCommonModule, MatButtonModule, MatDialogModule],
+  exports: [MatCommonModule, MatButtonModule, MatDialogModule],
 })
 export class MaterialModule {}
--- src/app/my-app/components/footer/footer.component.ts
+++ src/app/my-app/components/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PopUpComponent } from '../pop-up/pop-up.component';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog() {
+    this.dialog.open(PopUpComponent);
+  }
 }
